daf42a8498d941f4278318e848fbf5afaa2c8d1a
bit <bit@DESKTOP-ECQSVQA>
2018-03-30 21:48:37 +0900

    s3 이제 되나요

--- src/main/java/dev/mvc/util/S3Util.java
+++ src/main/java/dev/mvc/util/S3Util.java
@@ -1,13 +1,14 @@
-/*package dev.mvc.util;
+package dev.mvc.util;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
-import com.amazonaws.AmazonServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
 import com.amazonaws.auth.AWSCredentials;
@@ -23,8 +24,10 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 public class S3Util {
 	
-	private String accessKey = "보안보안보안"; // 엑세스 키
-	private String secretKey = "보안보안보안"; // 보안 엑세스 키
+	private static final Logger logger = LoggerFactory.getLogger(S3Util.class);
+	
+	private String accessKey = "AKIAITHB7I532AQ4IYNA"; // 엑세스 키
+	private String secretKey = "Foafxlpk20rDlNT4jB3Lk+gIh7g6VMAcjzpJMtd+"; // 보안 엑세스 키
 
 	private AmazonS3 conn;
 
@@ -55,14 +58,16 @@ public class S3Util {
 	public void fileUpload(String bucketName, String fileName, byte[] fileData) throws FileNotFoundException {
 
 		String filePath = (fileName).replace(File.separatorChar, '/'); // 파일 구별자를 `/`로 설정(\->/) 이게 기존에 / 였어도 넘어오면서 \로 바뀌는 거같다.
-		System.out.println("=============================="+filePath);
+		logger.info("파일 경로 : "+filePath);
+		
 		ObjectMetadata metaData = new ObjectMetadata();
-
 		metaData.setContentLength(fileData.length);   //메타데이터 설정 -->원래는 128kB까지 업로드 가능했으나 파일크기만큼 버퍼를 설정시켰다.
+	    logger.info("메타 데이터 : " + metaData);
+		
 	    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(fileData); //파일 넣음
-
+	    
 		conn.putObject(bucketName, filePath, byteArrayInputStream, metaData);
-
+		System.out.println("넌뭘까???????????????/"+filePath);
 	}
 
 	// 파일 삭제
@@ -74,14 +79,14 @@ public class S3Util {
 
 	// 파일 URL
 	public String getFileURL(String bucketName, String fileName) {
-		System.out.println("넘어오는 파일명 : "+fileName);
+		System.out.println("넘어오는 파일명111 : "+fileName);
 		String imgName = (fileName).replace(File.separatorChar, '/');
 		return conn.generatePresignedUrl(new GeneratePresignedUrlRequest(bucketName, imgName)).toString();
 	}
 	
 	// src파일 읽어오기
     public S3ObjectInputStream getSrcFile(String bucketName, String fileName) throws IOException{
-        System.out.println("넘어오는 파일명 : "+fileName);
+        System.out.println("넘어오는 파일명222 : "+fileName);
         fileName = (fileName).replace(File.separatorChar, '/');
         S3Object s3object = conn.getObject(new GetObjectRequest(bucketName, fileName)); //해당 파일 s3객체에 담기
         S3ObjectInputStream objectInputStream = s3object.getObjectContent();    //s3객체를 스트림으로 변환
@@ -91,4 +96,3 @@ public class S3Util {
 	
 	
 }
-*/
